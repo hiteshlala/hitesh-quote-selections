2a331d6301a11df518cf3d5e6095791544839857
hiteshlala <hiteshlala@gmail.com>
2016-05-30 19:55:05 -0700

    Displays Quote On Page When Testing
     Changes to be committed:
    	modified:   jsonptest/README.md
    	modified:   jsonptest/getQuotes.js
    	modified:   jsonptest/quoteForDay.html

--- jsonptest/README.md
+++ jsonptest/README.md
@@ -1,2 +1,5 @@
+# Testing JSONP Server
+
 While server is running open quoteForDay.html
-If all is working well then there will be a quote for the day printed in console
+Change the hostUrl in the getQuotes.js file to the server
+If all is working well then there will be a quote for the day printed on page
--- jsonptest/getQuotes.js
+++ jsonptest/getQuotes.js
@@ -5,17 +5,23 @@ var jsonpRequest = function(url, callback) {
   elem.appendChild(node);
 };
 
-
+// change this to wherver the server is being hosted
 var hostUrl = 'http://localhost:3030';
 
 jsonpRequest(hostUrl, function (data) {
-    console.log(data);
+    var elem = document.getElementById('container');
+    var node = document.createElement('p');
+    node.innerHTML = data.saying;
+    elem.appendChild(node);
+    node = document.createElement('p');
+    node.innerHTML = data.author;
+    elem.appendChild(node);
 });
 
-jsonpRequest(hostUrl, function (data) {
-     console.log(data.saying);
-});
+// jsonpRequest(hostUrl, function (data) {
+//      console.log(data.saying);
+// });
 
-jsonpRequest(hostUrl, function (data) {
-     console.log(data.author);
-});
+// jsonpRequest(hostUrl, function (data) {
+//      console.log(data.author);
+// });
